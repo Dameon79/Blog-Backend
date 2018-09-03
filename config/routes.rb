0b55599d5cddbe84648063fcbef1a9545a4356d1
Dameon Sumpton <33348922+Dameon79@users.noreply.github.com>
2018-09-03 14:18:10 +0100

    Feature (#10) (#11)
    * added admin MVC and basic MVC structure
    
    * completed the CRUD fuctionality for articles
    
    * Addeduser sign up option
    
    * fixed issue with form partial path
    
    * fixed validartion for title and text content of new article
    
    * created user Database
    
    * added bootstrap to gemfile
    
    * added bootstrap styling for admin namespace
    
    * added styling to admin view and updated gemfile with rpsec
    
    * installed rspec gem for testing
    
    * rolled back user db and added rpsec testing
    
    * rolled back db for users
    
    * added CRUD tests for articles
    
    * Addressed comments from first pr
    
    * added migration to include null false to articles DB
    
    * corrected root for admin namespace
    
    * added ruby version file to fix issues with codeship deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  root 'welcome#index'
+
+  namespace :admin do
+    root 'articles#index'
+    resources :articles
+  end
+
+  resources :articles
 end
