10a547360b2fa6d6c369070508b84f4843f21c70
Dameon Sumpton <Dsumpton@hotmail.co.uk>
2018-10-03 16:44:07 +0100

    added rspec to ensure service worker is queueing a job

--- spec/jobs/send_email_campaign_job_spec.rb
+++ spec/jobs/send_email_campaign_job_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe SendEmailCampaignJob, type: :worker do
   describe "sending an email" do
     it "should queue a worker" do
+      create(:article)
       expect {
         SendEmailCampaignJob.perform_async
       }.to change(SendEmailCampaignJob.jobs, :size).by(1)
