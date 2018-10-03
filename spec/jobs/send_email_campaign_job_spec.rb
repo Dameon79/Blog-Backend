dff410a9fa5e934325c5952fd8f3430bb1d1460f
Dameon Sumpton <Dsumpton@hotmail.co.uk>
2018-10-03 16:11:09 +0100

    added rspec to ensure service worker is queueing a job

--- spec/jobs/send_email_campaign_job_spec.rb
+++ spec/jobs/send_email_campaign_job_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe SendEmailCampaignJob, type: :worker do
   describe "sending an email" do
     it "should queue a worker" do
-      expect (SendEmailCampaignJob).to have_enqueued_sidekiq_job('SendEmailCampaignJob', true)
+      expect {
+        SendEmailCampaignJob.perform_async(1, 2)
+      }.to change(SendEmailCampaignJob.jobs, :size).by(1)
+    end
   end
 end
--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -1,2 +1,9 @@
-require 'sidekiq/testing'
-Sidekiq::Testing.fake!
+RSpec::Sidekiq.configure do |config|
+  config.warn_when_jobs_not_processed_by_sidekiq = false
+end
+
+RSpec.configure do |config|
+  config.before(:each) do
+    Sidekiq::Worker.clear_all
+  end
+end
