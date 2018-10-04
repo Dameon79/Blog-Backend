b65653238f4fcb94954135c9aed6292b495f4776
Dameon Sumpton <Dsumpton@hotmail.co.uk>
2018-10-04 12:24:47 +0100

    updated sidekiq.rb helper

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -10,10 +10,18 @@ RSpec.configure do |config|
   end
 end
 
- RSpec.configure do |config|
-   config.around(:each, :inline) do
-       Sidekiq::Testing.inline!
-     else
-       Sidekiq::Testing.fake!
+RSpec.configure do |config|
+  config.profile_examples = true
+  config.before(:suite) do
+    Sidekiq::Testing.fake!
+  end
+ 
+  config.around(:example, sidekiq: -> (mode) { !!mode }) do |example|
+    unless %i(inline fake disable).include?(example.metadata.fetch(:sidekiq))
+      raise "sidekiq_mode must be :inline, :fake or :disable"
+    end
+    Sidekiq::Testing.send("#{example.metadata.fetch(:sidekiq)}!".to_sym) do
+      example.run
     end
   end
+ end
