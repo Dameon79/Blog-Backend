26a903dac5f91da3ced5dabb1e56ecb4f2e79d99
Dameon Sumpton <Dsumpton@hotmail.co.uk>
2018-11-07 12:03:35 +0000

    fixed merge conflicts

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,8 +1,4 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
-<<<<<<< HEAD
-=======
-    
->>>>>>> e8364b55aa24bf792fd5f3b19ed3e1b3fc40b31f
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,33 +1,20 @@
 module Types
   class QueryType < Types::BaseObject
-<<<<<<< HEAD
     include Pagy::Backend
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
     field :article, ArticleType, null: true do
-=======
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    field :article, ArticleType , null: true do
->>>>>>> e8364b55aa24bf792fd5f3b19ed3e1b3fc40b31f
       description "Find an article by id"
       argument :id, ID, required: true
     end
 
-<<<<<<< HEAD
     field :articles, Types::ArticleType.connection_type, null: false do
       argument :page, Int, required: false
-=======
-    field :articles, [ArticleType], null: true do
-      description "Returns an array of all articles"
->>>>>>> e8364b55aa24bf792fd5f3b19ed3e1b3fc40b31f
     end
 
     def article(id:)
       Article.find(id)
-<<<<<<< HEAD
     end 
 
     def articles(page: nil)
@@ -44,12 +31,6 @@ module Types
 
     def params
       @params ||= {}
-=======
-    end
-
-    def articles
-      Article.all
->>>>>>> e8364b55aa24bf792fd5f3b19ed3e1b3fc40b31f
     end
   end
 end
